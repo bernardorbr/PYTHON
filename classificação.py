2756efa6e565fb9ee494e599d361618511c16c4e
Bernardo <salesribeirobernardo8@gmail.com>
2026-09-23 13:27:02 -0300

    Classificação de Turma

--- classificação.py
+++ classificação.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 a= input('Me diga seu nome: \n')
 b= float(input("Me informe sua média geral do 1ª Bimestre: "))
 c= float(input("Me informe a sua média de geral do 2ª Bimestre: "))
@@ -13,16 +13,4 @@ elif media < 8 and media > 6:
     
 else:
     print("você está na turma C")
-=======
-name = input('Digite seu nome: ')
-a = int(input('Digite a primeria nota: '))
-b = int(input('Digite a segunda nota: '))
-c = int(input('Digite a terceira nota: '))
-media= (a+b+c)/3
-if media <6:
-  print('Reprovado',name,  media) 
-elif media <6 and media >4:
-  print('Recuperação')
-else:
-  print ('Aprovado',name,  media)
->>>>>>> aff3f95 (Sistema de senha)
+
